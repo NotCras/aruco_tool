c78255484e6045534dc5000d5f9bb6ff40fae68c
john <morrowjo@oregonstate.edu>
2022-04-06 09:22:09 -0700

    added camera calibration helper function

--- aruco_tool/aruco_helper.py
+++ aruco_tool/aruco_helper.py
@@ -3,6 +3,7 @@ from cv2 import aruco
 import pdb
 import pandas as pd
 import numpy as np
+import glob
 from .corner_finder import CornerFinder
 from .aruco_corner import ArucoCorner
 from .aruco_loc import ArucoLoc
@@ -85,7 +86,8 @@ class ArucoHelper:
     @staticmethod
     def view_data_video(acode, include_corners=False):
         """
-        Shows image stream as a video. Useful for debugging. 
+        Shows image stream as a video. Useful for debugging. Trying to only use opencv for this.
+        Will ignore function for now...
         """
         try:
             # get folder location of the aruco code
@@ -94,7 +96,69 @@ class ArucoHelper:
             raise Exception("ArucoCorner object does not have a folder location associated with it")
 
         pass
+    
 
+    @staticmethod
+    def camera_calibrate(folder_loc):
+        """Helper script that runs camera calibration for opencv.
+
+        Args:
+            folder_loc (string): folder location that contains all the images for camera calibration. 
+        """
         
-        
+        # Defining the dimensions of checkerboard
+        CHECKERBOARD = (6,9)
+        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+        # Creating vector to store vectors of 3D points for each checkerboard image
+        objpoints = []
+        # Creating vector to store vectors of 2D points for each checkerboard image
+        imgpoints = [] 
+
+
+        # Defining the world coordinates for 3D points
+        objp = np.zeros((1, CHECKERBOARD[0] * CHECKERBOARD[1], 3), np.float32)
+        objp[0,:,:2] = np.mgrid[0:CHECKERBOARD[0], 0:CHECKERBOARD[1]].T.reshape(-1, 2)
+        prev_img_shape = None
+
+        # Extracting path of individual image stored in a given directory
+        images = glob.glob(f"{folder_loc}/*.jpg")
+        for fname in images:
+            img = cv2.imread(fname)
+            gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+            # Find the chess board corners
+            # If desired number of corners are found in the image then ret = true
+            ret, corners = cv2.findChessboardCorners(gray, CHECKERBOARD, cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_FAST_CHECK + cv2.CALIB_CB_NORMALIZE_IMAGE)
+            
+            """
+            If desired number of corner are detected,
+            we refine the pixel coordinates and display 
+            them on the images of checker board
+            """
+            if ret == True:
+                objpoints.append(objp)
+                # refining pixel coordinates for given 2d points.
+                corners2 = cv2.cornerSubPix(gray, corners, (11,11),(-1,-1), criteria)
+                
+                imgpoints.append(corners2)
+
+                # Draw and display the corners
+                img = cv2.drawChessboardCorners(img, CHECKERBOARD, corners2, ret)
+            
+            cv2.imshow('img',img)
+            cv2.waitKey(0)
+
+        cv2.destroyAllWindows()
+
+        h,w = img.shape[:2]
+
+        """
+        Performing camera calibration by 
+        passing the value of known 3D points (objpoints)
+        and corresponding pixel coordinates of the 
+        detected corners (imgpoints)
+        """
+        ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+
+        return mtx, dist
 
